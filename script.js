b4f73ac91a798b7d6870fb936468bdf8b2a2ce76
SamSchroder123 <samueltillotsonschroder@gmail.com>
2024-06-19 14:50:04 +0100

    updated script.js with comments and removed console log

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 async function calculateCharge() {
-  const fixedFeeUSD = 0.49;
+  const fixedFeeUSD = 0.49; //update if the fixed fee for paypal checkout changes
   const customerChargeZAR = document.getElementById("customerCharge").value;
-  const conversionRate = await fetchConversionRate();
-  const fixedFeeZAR = fixedFeeUSD / conversionRate;
-  const baseChargeZAR = (customerChargeZAR - fixedFeeZAR) / 1.0349;
-  const baseChargeUSD = baseChargeZAR * conversionRate;
+  const conversionRate = await fetchConversionRate(); //get the conversion rate from coinbase
+  const fixedFeeZAR = fixedFeeUSD / conversionRate; //convert the fixed fee to ZAR
+  const baseChargeZAR = (customerChargeZAR - fixedFeeZAR) / 1.0349; //1.0349 is the percentage rate for receiving domestic transactions
+  const baseChargeUSD = baseChargeZAR * conversionRate; //convert the base charge to USD
 
   // Detailed breakdown for transparency
   let detailedResult = `
@@ -25,6 +25,6 @@ async function fetchConversionRate() {
     "https://api.coinbase.com/v2/exchange-rates?currency=ZAR"
   );
   const data = await response.json();
-  console.log(data);
+  //console.log(data);
   return parseFloat(data.data.rates.PYUSD);
 }
